04164ac3b1e6f7cf4d6f7d7c7c5e97782befa250
smonzon14 <smonzon360@gmail.com>
2025-10-06 18:41:37 -0400

    Title fixes

--- src/app/jelly-text.tsx
+++ src/app/jelly-text.tsx
@@ -2,14 +2,14 @@ import { ReactNode } from "react";
 
 export const JellyText = ({ children }: { children: ReactNode | string }) => {
   return (
-    <div className="relative overflow-hidden inline-block text-4xl sm:text-5xl md:text-7xl lg:text-9xl text-white">
+    <div className="relative overflow-hidden inline-block text-5xl sm:text-6xl md:text-7xl lg:text-9xl text-white">
       <video
         preload="auto"
         autoPlay
         loop
         muted
         playsInline
-        className="w-full h-full absolute top-0 object-cover z-0"
+        className="w-full h-full absolute top-0 object-cover z-0 pt-1"
         style={{ filter: "brightness(1.6)" }}
         // Hint to browser for priority loading (not a guarantee)
         data-priority="true"
