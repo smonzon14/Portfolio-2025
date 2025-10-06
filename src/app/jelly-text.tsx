ab5a1a84f4166580a2a31ac5af7a9226f54ee681
smonzon14 <smonzon360@gmail.com>
2025-10-06 15:35:11 -0400

    Update loading adn soundcloud

--- src/app/jelly-text.tsx
+++ src/app/jelly-text.tsx
@@ -5,11 +5,19 @@ import { ReactNode } from 'react';
 export const JellyText = ({ children }: { children: ReactNode | string}) => {
     return (
         <div className="relative overflow-hidden inline-block text-4xl sm:text-5xl md:text-7xl lg:text-9xl text-white">
-            <video preload='auto' autoPlay loop muted playsInline className="w-full h-full absolute top-0 object-cover z-0" style={{ filter: "brightness(1.8)" }}>
-                <source src="jellies2.mp4" type="video/mp4">
-                </source>
-            </video>
-            <div className="width-full mix-blend-multiply z-10 bg-[var(--background)]">
+            <video
+                preload="auto"
+                autoPlay
+                loop
+                muted
+                playsInline
+                className="w-full h-full absolute top-0 object-cover z-0"
+                style={{ filter: "brightness(1.6)" }}
+                // Hint to browser for priority loading (not a guarantee)
+                data-priority="true"
+            >
+                <source src="jellies2.mp4" type="video/mp4" />
+            </video>  <div className="width-full mix-blend-multiply z-10 bg-[var(--background)]">
                 {children}
             </div>
         </div>
